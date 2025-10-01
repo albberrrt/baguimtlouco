d5649415720821547303d9c41d78ef328f9fac82
albberrrt <albertsmaczylo@gmail.com>
2025-10-01 11:05:00 -0300

    resolvendo bugs mt loucos

--- main.lua
+++ main.lua
@@ -1,52 +1,58 @@
 function love.load()
   width, height = 50, 50
   x, y = love.graphics.getWidth() / 2 - width / 2, love.graphics.getHeight() / 2 - height / 2
-  speed = 0
-  acceleration = 0.1
-  maxSpeed = 5
-  direction = 0       -- -1: left, 1: right, 0: idle
-  facingDirection = 1 -- -1: left, 1: right
   love.graphics.setBackgroundColor(52 / 255, 76 / 255, 235 / 255)
+
+  velocity = 0
+  speed = 0
+  maxSpeed = 5
+
+  input_dir = 1 -- -1: left, 1: right
+  acceleration = 0.01
+  friction = 0.05
+end
+
+function getKeyDirection()
+  if love.keyboard.isDown("right") then
+    return 1
+  elseif love.keyboard.isDown("left") then
+    return -1
+  else
+    return 0
+  end
+end
+
+function accelerate(dir)
+  velocity = velocity + (acceleration * dir)
+
+  if speed > maxSpeed then
+    velocity = maxSpeed * input_dir
+  end
 end
 
 function love.update(dt)
-  if love.keyboard.isDown("right") then
-    direction = 1
-    if speed <= maxSpeed then
-      speed = speed + acceleration
-    end
-    -- x = x + speed
-  end
-  if love.keyboard.isDown("left") then
-    direction = -1
-    if speed <= maxSpeed then
-      speed = speed + acceleration
-    end
-    -- x = x - speed
-  end
-  if not love.keyboard.isDown("right") and not love.keyboard.isDown("left") then
-    if speed > 0 then
-      direction = direction -- maintain direction while slowing down
-      speed = speed - (acceleration)
-    end
-    if speed == 0 then
-      direction = 0
-    end
-  end
-  x = x + (speed * direction)
+  speed = math.abs(velocity) -- 100 is the proportion factor
 
-  if x <= 0 or x >= 750 then          -- 750 is 800 (window width) - 50 (rectangle width)
-    x = math.max(0, math.min(x, 750)) -- Clamp x between 0 and 750
-    speed = 0
+  input_dir = getKeyDirection()
+  if input_dir ~= 0 then
+    accelerate(input_dir)
   end
-  if speed < 0 then
-    speed = 0
-  end
+  -- -- Deceleration
+  -- if not love.keyboard.isDown("right") and not love.keyboard.isDown("left") then
+  --   if math.abs(speed) > 0 then
+  --     speed = speed + (acceleration * -direction)
+  --   end
+  -- end
+
+  -- -- Clamp position and speed
+  -- if x <= 0 or x >= 750 then          -- 750 is 800 (window width) - 50 (rectangle width)
+  --   x = math.max(0, math.min(x, 750)) -- Clamp x between 0 and 750
+  -- end
 end
 
 function love.draw()
   love.graphics.setColor(1, 1, 1)
   love.graphics.rectangle("fill", x, y, width, height)
 
-  love.graphics.print("Direction: " .. direction .. " Speed: " .. speed, 10, 10)
+  love.graphics.print("Input direction: " .. input_dir .. " Speed: " .. speed, 10, 10)
 end
